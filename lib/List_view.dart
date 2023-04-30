a221858309072fa280fb258c315d60e02af76aa4
Rayder-R <71495943+Rayder-R@users.noreply.github.com>
2023-04-30 21:40:11 +0800

    <1>

--- lib/List_view.dart
+++ lib/List_view.dart
@@ -18,24 +18,22 @@ class _MyListView extends State<MyListView> {
 
   var initCounter = 0;
   var plusCounter = 10;
-
-  final List MyListItems = List<String>.generate(26, (i) {
-    return "Item: $i";
-  });
+  bool _isLoading = false;
   var items = [];
 
+  List MyListItems = List<String>.generate(26, (i) {
+    i = i+1; // index 0~1
+    return "Item: $i";
+  });
 
-  bool _isLoading = false;
+  Widget useLoading() {
+    return LoadingAnimationWidget.stretchedDots(color: Colors.yellow, size: 100);
+  }
 
   void show() {
     setState(() {
       _isLoading = true;
-
     });
-  }
-
-  Widget useLoading() {
-    return LoadingAnimationWidget.stretchedDots(color: Colors.yellow, size: 100);
   }
 
   void hide() {
@@ -44,62 +42,55 @@ class _MyListView extends State<MyListView> {
     });
   }
 
-
-  //TODO State狀態
-  //init 是在剛開始創建State就會被呼叫一次
+  //TODO initState狀態是在剛開始創建State就會被呼叫一次
   @override
   void initState() {
     super.initState();
-    //TODO
-    print('initState');
+
+    print('initState()');
     _update_listdata();
+
+    String message = MyListItems.length.toString();
+    print("讀取資料總比數: $message");
   }
 
-
+  //TODO Loading
   // ignore: unused_element
-  void _loading_item() async {
+  Future _loading_item() async {
     setState(() {
       show();
-      print(_isLoading);
-      // const Loading_Item();
+      print("1.setState $_isLoading");
     });
+
     await Future.delayed(const Duration(seconds: 3), hide);
-
-
-
-    print(_isLoading);
-
-    print("await");
+    print("2.setState $_isLoading");
   }
+  Future _update_dataModle() async {
 
-
-  Future<int> futureMethod1(){
-    return Future.delayed(Duration(seconds: 1), ()=>10);
-  }
-
-  void _update_listdata()  {
-    //TODO setState
-    setState(() {
-        print('_update_listdata');
-
-        if(MyListItems.length >= (items.length + plusCounter)){
-        items.addAll(MyListItems.getRange(initCounter, initCounter + plusCounter));
-        initCounter = initCounter + plusCounter;
-
-        }else{
+    ///讀取原本的 [MyListItems] List資料 addAll進顯示用 [items] List 資料
+    Future.value(true).then((value) {
+      return setState(() {
+        print('_update_listdata()');
+        if (MyListItems.length >= (items.length + plusCounter)) {
+          items.addAll(
+              MyListItems.getRange(initCounter, initCounter + plusCounter));
+          initCounter = initCounter + plusCounter;
+        } else {
           items.addAll(MyListItems.getRange(initCounter, MyListItems.length));
           initCounter = initCounter + (MyListItems.length - initCounter);
-
         }
 
-        print(initCounter);
-        print(items);
-        print(MyListItems.length);
-
+        print("目前已加入總比數: $initCounter");
+        print("目前顯示資料:  $items ");
+      });
     });
 
   }
 
+  void _update_listdata() async {
+    await _loading_item();
+    _update_dataModle();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -120,17 +111,20 @@ class _MyListView extends State<MyListView> {
               title: Text(items[index]),
             ),
 
-            ((index+1 == initCounter) & (items.length < MyListItems.length)) ?
+            ((index+1 == initCounter) & (items.length < MyListItems.length) & (!_isLoading)) ?
             Container(
               child: TextButton(
-                onPressed: _loading_item,
-                // (_isLoading)? LoadingAnimationWidget.stretchedDots(color: Colors.yellow, size: 100):,
+                onPressed: _update_listdata,
                 child: Text("顯示更多"),
               )
             ) :
             Container(),
-            (_isLoading)?
-            useLoading():
+
+
+          ((index+1 == initCounter) & (items.length < MyListItems.length) &(_isLoading))
+              ?
+              useLoading()
+              :
             Container(),
           ]
           )
